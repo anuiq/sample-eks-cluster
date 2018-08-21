58a252ba38b5ca7fea3b35cf5704a9c4fdcf48f8
Ishneet Dua <isha212dua@gmail.com>
2018-08-21 14:47:05 +0530

    Changed account numbers

--- starter-script.sh
+++ starter-script.sh
@@ -27,16 +27,16 @@ setup_heptio_authenticator_aws(){
 check_aws_credentials(){
   account_number="$(aws sts get-caller-identity --output text --query 'Account')"
   case "${account_number}" in
-    121660731498)
+    XXXXXXXX)
       echo "You are using the *****AWS sandbox***** account"
       ;;
-    180165713404)
+    XXXXXXXX)
       echo "You are using ****cdk-aws-digital-business-nonprod **** account"
       ;;
-    828373668907)
+    XXXXXXXX)
       echo "You are using ****cdk-aws-dmg-nitra-prod**** account"
       ;;
-    140358921891)
+    XXXXXXXX)
       echo "You are using ****ghs_platform_cloud**** account"
       ;;
     *)
